4fa036e8cde14a8eb53682d1ca7cdd175b6ee49e
fahadarsshad <fhdarshad57@gmail.com>
2015-11-28 12:28:30 +0500

    design setting

--- application/views/templates/address.php
+++ application/views/templates/address.php
@@ -9,6 +9,12 @@
 							<li>5c/1 Madina Street Moon Market Allama Iqbal Town</li>
 						</ul>
 						
+						<div class="call_now">
+					<a>
+		    			<img class="img-responsive" src="<?php echo base_url().'assets/images/call now_red.png'?>" />
+		  			</a>
+				</div>
+						
 						<ul><b>Phone:</b>
 							<li>Tel-1: 03217474747</li>
 							<li>Tel-2: 03007474747</li>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,13 +1,15 @@
 
-	<footer class="row">
+	<div class="page-footer row">
 		<nav class="col-lg-12">
 			<ul class="breadcrumb">
 				<li><a href="">Touch With Us</a></li>
 				<li><a href="">Why We are Better</a></li>
-				<li><a href="">FeedBack</a></li>
+				<li><a href="">FeedBack</a></li><br />
+				<li><a href="">Copyright © 2015 heavencaterers | Website Development by Fahad Arshad</a></li>
+				
 			</ul>
 		</nav>
-	</footer>
+	</div>
 </div><!--container wrapper-->
 	</body>
 </html>
